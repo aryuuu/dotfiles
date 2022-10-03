f9add3e6684569ee46cdc7a44be074c7dfabd8b5
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-10-03 09:17:06 +0700

    feat: add script to set and unset default protocol for git with github

--- scripts/.scripts/hzndp1.sh
+++ scripts/.scripts/hzndp1.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x781 --rotate normal --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+MON2_CONNECTED=$(xrandr | grep ' connected' | grep 'DP-1' | awk '{print $1}')
+[[ ! -z "$MON2_CONNECTED" ]] && xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x781 --rotate normal --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal && i3-msg restart
