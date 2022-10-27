9af2e90331ab612e3968bbf52e8ca58c5ea198a8
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-10-27 17:09:35 +0700

    feat: add filename to lualine

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -1,10 +1,10 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-    return
+	return
 end
 
 local hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
+	return vim.fn.winwidth(0) > 80
 end
 
 -- local function hello_world()
@@ -13,105 +13,124 @@ end
 -- end
 
 local function is_venn_enabled()
-    local venn_enabled = vim.inspect(vim.b.venn_enabled)
-    if venn_enabled == "nil" then
-        return "x"
-    end
+	local venn_enabled = vim.inspect(vim.b.venn_enabled)
+	if venn_enabled == "nil" then
+		return "x"
+	end
 
-    return "v"
+	return "v"
 end
 
+local filename = {
+	"filename",
+	file_status = true, -- Displays file status (readonly status, modified status)
+	newfile_status = false, -- Display new file status (new file means no write after created)
+	path = 0, -- 0: Just the filename
+	-- 1: Relative path
+	-- 2: Absolute path
+	-- 3: Absolute path, with tilde as the home directory
+
+	shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+	-- for other components. (terrible name, any suggestions?)
+	symbols = {
+		modified = "[+]", -- Text to show when the file is modified.
+		readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+		unnamed = "[No Name]", -- Text to show for unnamed buffers.
+		newfile = "[New]", -- Text to show for new created file before first writting
+	},
+}
+
 local diagnostics = {
-    "diagnostics",
-    sources = { "nvim_diagnostic" },
-    sections = { "error", "warn" },
-    symbols = { error = "E ", warn = "W " },
-    colored = false,
-    update_in_insert = false,
-    always_visible = true,
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = "E ", warn = "W " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
 }
 
 local diff = {
-    "diff",
-    colored = false,
-    diff_color = {
-        added = "green",
-        modified = "yellow",
-        removed = "red",
-    },
-    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-    cond = hide_in_width,
+	"diff",
+	colored = false,
+	diff_color = {
+		added = "green",
+		modified = "yellow",
+		removed = "red",
+	},
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	cond = hide_in_width,
 }
 
 local mode = {
-    "mode",
-    fmt = function(str)
-        return "-- " .. str .. " --"
-    end,
+	"mode",
+	fmt = function(str)
+		return "-- " .. str .. " --"
+	end,
 }
 
 local filetype = {
-    "filetype",
-    icons_enabled = false,
-    icon = nil,
+	"filetype",
+	icons_enabled = false,
+	icon = nil,
 }
 
-local filename = {
-    "filename",
-    path = 1,
-}
+-- local filename = {
+-- 	"filename",
+-- 	path = 1,
+-- }
 
 local branch = {
-    "branch",
-    icons_enabled = true,
-    icon = "",
+	"branch",
+	icons_enabled = true,
+	icon = "",
 }
 
 local location = {
-    "location",
-    padding = 0,
+	"location",
+	padding = 0,
 }
 
 -- cool function for progress
 local progress = function()
-    local current_line = vim.fn.line(".")
-    local total_lines = vim.fn.line("$")
-    local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-    local line_ratio = current_line / total_lines
-    local index = math.ceil(line_ratio * #chars)
-    return chars[index]
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return chars[index]
 end
 
 local spaces = function()
-    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
 lualine.setup({
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
-        always_divide_middle = true,
-    },
-    sections = {
-        lualine_a = { branch, diagnostics },
-        lualine_b = { mode, is_venn_enabled },
-        lualine_c = { "lsp_progress" },
-        -- lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_x = { diff, spaces, "encoding", filetype },
-        lualine_y = { location },
-        lualine_z = { progress },
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-    },
-    tabline = {},
-    extensions = {},
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { branch, diagnostics },
+		lualine_b = { mode, is_venn_enabled },
+		lualine_c = { "lsp_progress", filename },
+		-- lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_y = { location },
+		lualine_z = { progress },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
 })
