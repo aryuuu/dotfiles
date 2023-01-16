528ee102b2ef1cf78a43cf92eb778f2292650a15
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-01-16 21:01:12 +0700

    feat: auto indent on after moving line

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -87,8 +87,8 @@ keymap("v", "<leader>S", "\"fy/\\V<C-R>f<CR>", opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>==gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>==gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
