ac5c51857617cde241475fc2e0d8d9e5c40640a0
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-01-28 08:41:04 +0700

    feat: restart i3 on smon

--- scripts/.scripts/smon.sh
+++ scripts/.scripts/smon.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off && i3-msg restart
+
+xinput map-to-output "ELAN2513:00 04F3:2AF1" eDP-1
