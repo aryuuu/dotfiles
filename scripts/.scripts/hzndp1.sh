acfc0d4c257a8ed363c546af96b05c492b5d5ae9
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-02-02 19:40:01 +0700

    feat: adjust monitor position in hzndp1

--- scripts/.scripts/hzndp1.sh
+++ scripts/.scripts/hzndp1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 MON2_CONNECTED=$(xrandr | grep ' connected' | grep 'DP-1' | awk '{print $1}')
-[[ ! -z "$MON2_CONNECTED" ]] && xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x781 --rotate normal --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal && i3-msg restart
+[[ ! -z "$MON2_CONNECTED" ]] && xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x100 --rotate normal --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal && i3-msg restart
 
 xinput map-to-output "ELAN2513:00 04F3:2AF1" eDP-1
