e799f2ab033cb80e4a3e59b0dcecc0c7d50c8efb
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-03-23 09:23:29 +0700

    feat: add laymon.sh

--- scripts/.scripts/laymon.sh
+++ scripts/.scripts/laymon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-i3-msg "workspace 13; append_layout ~/.config/i3-layout-manager/layouts/layout-MON3.json; exec i3-sensible-terminal; exec i3-sensible-terminal; exec i3-sensible-terminal; exec i3-sensible-terminal; exec i3-sensible-terminal"
+i3-msg "workspace 13:; append_layout ~/.config/i3-layout-manager/layouts/layout-MON3.json; exec i3-sensible-terminal; exec i3-sensible-terminal; exec i3-sensible-terminal; exec i3-sensible-terminal; exec i3-sensible-terminal"
