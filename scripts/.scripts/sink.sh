ddce9f6948da32c472e3c26af51dc87d48adaf78
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-04-14 09:48:31 +0700

    feat: center dmenu

--- scripts/.scripts/sink.sh
+++ scripts/.scripts/sink.sh
@@ -14,7 +14,7 @@ DMENU_SHF="#FA485D"
 DMENU_NHB="#282A36"
 DMENU_NHF="#FA485D"
 
-sink=$(ponymix -t sink list|awk '/^sink/ {s=$1" "$2;getline;gsub(/^ +/,"",$0);print s" "$0}'|dmenu -i -fn "FontAwesome" -p  -l 5 -sb $DMENU_SB -sf $DMENU_SF -nb $DMENU_NB -nf $DMENU_NF -shb $DMENU_SHB -shf $DMENU_SHF -nhb $DMENU_NHB -nhf $DMENU_NHF |grep -Po '[0-9]+(?=:)') &&
+sink=$(ponymix -t sink list|awk '/^sink/ {s=$1" "$2;getline;gsub(/^ +/,"",$0);print s" "$0}'|dmenu -c -i -fn "FontAwesome" -p  -l 5 -sb $DMENU_SB -sf $DMENU_SF -nb $DMENU_NB -nf $DMENU_NF -shb $DMENU_SHB -shf $DMENU_SHF -nhb $DMENU_NHB -nhf $DMENU_NHF |grep -Po '[0-9]+(?=:)') &&
 
 ponymix set-default -d $sink &&
 for input in $(ponymix list -t sink-input|grep -Po '[0-9]+(?=:)');do
