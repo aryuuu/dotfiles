23da47b8ba57062ba4019facf5c4666c3b090c64
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-07-24 18:17:49 +0700

    feat: add caching do dmenu-github

--- scripts/.scripts/dmenu-github.sh
+++ scripts/.scripts/dmenu-github.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# uncomment for debugging
+# set -o xtrace
+
 GITHUB_TOKEN=$(<~/.config/gh/dmenu_github_token)
 
-query=$(echo " " | dmenu -c -fn "FontAwesome" -p " "  -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -shb '#6272A4' -shf '#FA485D' -nhb '#282A36' -nhf '#FA485D' | tr ' ' '+' )
+query=$(cat ~/.cache/dmenu-github | dmenu -c -fn "FontAwesome" -p " "  -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -shb '#6272A4' -shf '#FA485D' -nhb '#282A36' -nhf '#FA485D' | tr ' ' '+' )
 
 if [[ -z $query ]];
 then
@@ -10,13 +13,26 @@ then
     exit 1
 fi
 
-selected_repo=$(curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/search/repositories\?q\=$query \
-  | jq '.items[] | .full_name' \
-  | tr -d '"' \
-  | dmenu -c -fn "FontAwesome" -p " "  -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -shb '#6272A4' -shf '#FA485D' -nhb '#282A36' -nhf '#FA485D')
+selected_repo=''
+if grep -Fxq $query ~/.cache/dmenu-github; then
+    selected_repo=$query
+else
+    selected_repo=$(curl -L \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $GITHUB_TOKEN" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/search/repositories\?q\=$query \
+      | jq '.items[] | .full_name' \
+      | tr -d '"' \
+      | dmenu -c -fn "FontAwesome" -p " "  -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -shb '#6272A4' -shf '#FA485D' -nhb '#282A36' -nhf '#FA485D')
 
-xdg-open https://github.com/$selected_repo
+    if [[ -z $selected_repo ]];
+    then
+        notify-send "dmenu github" "Please select a repo"
+        exit 1
+    fi
+
+    echo $selected_repo >> ~/.cache/dmenu-github
+fi
+
+xdg-open https://github.com/$selected_repo 2> /dev/null
