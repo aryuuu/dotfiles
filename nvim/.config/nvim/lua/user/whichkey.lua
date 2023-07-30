1d2da03e02e1b55b9a0cede354323694403fe4ab
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-07-30 11:54:15 +0700

    feat: migrate from dmenu to rofi

--- nvim/.config/nvim/lua/user/whichkey.lua
+++ nvim/.config/nvim/lua/user/whichkey.lua
@@ -127,6 +127,7 @@ local mappings = {
 		"Find files",
 	},
 	["F"] = { "<cmd>Telescope grep_string search= theme=ivy only_sort_text=true<cr>", "Find Text" },
+	-- TODO: make this fuzzy
 	["/"] = { "<cmd>Telescope live_grep search= theme=ivy only_sort_text=true search_dirs={'%:p'}<cr>", "Find Text" },
 	-- ["/"] = { "<cmd>Telescope grep_string search= theme=ivy only_sort_text=true search_dirs={'%:p'}<cr>", "Find Text" },
 	["T"] = { "<cmd>TransparentToggle<cr>", "Toggle Transparency" },
