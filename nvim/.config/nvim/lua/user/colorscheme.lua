2c311e55f0872322b1244363bf89cefd268baf6d
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-12-02 08:02:40 +0700

    feat: update config

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 try
-  colorscheme nordfox
+  colorscheme onenord
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- scripts/.scripts/hzndp1.sh
+++ scripts/.scripts/hzndp1.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 MON2_CONNECTED=$(xrandr | grep ' connected' | grep 'DP-1' | awk '{print $1}')
 [[ ! -z "$MON2_CONNECTED" ]] && xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x100 --rotate normal --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal && i3-msg restart && notify-send "i3" "horizontal dual display"
 
