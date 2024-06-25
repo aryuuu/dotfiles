224de36a02311d8e7917ffbb09f7e994faa3e3cd
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-06-25 15:42:05 +0700

    feat: update toggleterm

--- nvim/.config/nvim/lua/user/toggleterm.lua
+++ nvim/.config/nvim/lua/user/toggleterm.lua
@@ -4,7 +4,13 @@ if not status_ok then
 end
 
 toggleterm.setup({
-    size = 50,
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+    end,
     open_mapping = [[<c-\>]],
     hide_numbers = true,
     shade_filetypes = {},
@@ -55,12 +61,6 @@ local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
 
 function _NCDU_TOGGLE()
     ncdu:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-    htop:toggle()
 end
 
 local python = Terminal:new({ cmd = "python", hidden = true })
