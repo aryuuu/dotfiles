554113740cf9446a732d6527b089592c01aaccb3
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-06-27 10:23:20 +0700

    feat: update config

--- nvim/.config/nvim/lua/user/gitsigns.lua
+++ nvim/.config/nvim/lua/user/gitsigns.lua
@@ -39,13 +39,6 @@ gitsigns.setup({
 		changedelete = { text = "~-" },
 		untracked = { text = "┆" },
 	},
-	-- signs = {
-	--     add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-	--     change = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-	--     delete = { hl = "GitSignsDelete", text = "-", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-	--     topdelete = { hl = "GitSignsDelete", text = "--", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-	--     changedelete = { hl = "GitSignsChange", text = "~-", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-	-- },
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
