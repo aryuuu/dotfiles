7406109f7689f770677dac9cf2f55708c3f1a47e
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-08-16 08:12:06 +0700

    feat: use new gitlinker plugin

--- nvim/.config/nvim/lua/user/gitlinker.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, gitlinker = pcall(require, "gitlinker")
-if not status_ok then
-    return
-end
-
-gitlinker.setup()
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -251,8 +251,13 @@ local plugins = {
 	-- Git
 	"lewis6991/gitsigns.nvim",
 	{
-		"ruifm/gitlinker.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
+		"linrongbin16/gitlinker.nvim",
+		cmd = "GitLink",
+		opts = {},
+		keys = {
+			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+		},
 	},
 	"tpope/vim-fugitive",
 	"tpope/vim-rhubarb",
