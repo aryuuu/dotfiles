b3827bddeadd0135745ba61a9d4d01aba7158c69
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2025-09-09 17:27:13 +0700

    feat: resize autocmd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -195,6 +195,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd(
+    'VimResized',
+    {
+        pattern = '*',
+        command = 'wincmd =',
+        desc = 'Automatically resize windows when the host window size changes.'
+    }
+)
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
