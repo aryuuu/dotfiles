89f3801e44b9100ddbe29616e36c47641ec03f64
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2025-11-18 08:08:17 +0700

    fix: use native lsp goto def instead of telescope (there's uri related bug)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -827,7 +827,7 @@ require('lazy').setup({
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
@@ -954,6 +954,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         pyright = {},
+        zls = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -961,7 +962,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {}
+        ts_ls = {},
+        eslint_d = {},
 
         lua_ls = {
           -- cmd = { ... },
